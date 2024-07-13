7f94a498125b654dbf6b21eee6bef50022f00fb4
Kriti Raj <6517kritiraj@gmail.com>
2024-07-13 18:32:08 +0530

    server load reduced: removed console.log when connection and disconnection of user

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -20,7 +20,7 @@ const userSocketMap = {}; // {userId: socketId}
 
 
 io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
+    // console.log("a user connected", socket.id);
 
     const userId = socket.handshake.query.userId;
     if (userId != "undefined") userSocketMap[userId] = socket.id;
@@ -30,7 +30,7 @@ io.on("connection", (socket) => {
 
     // socket.on() is used to listen to the events. can be used both on client and server side
     socket.on("disconnect", () => {
-        console.log("user disconnected", socket.id);
+        // console.log("user disconnected", socket.id);
         delete userSocketMap[userId];
         io.emit("getOnlineUsers", Object.keys(userSocketMap));
     })
