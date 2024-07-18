6d647e8a8930ad6c30b8c05ac25ba9ae1a6df72a
Kriti Raj <6517kritiraj@gmail.com>
2024-07-19 04:01:20 +0530

    typo resolved

--- backend/db/connectTOMongoDB.js
+++ backend/db/connectTOMongoDB.js
@@ -5,7 +5,7 @@ const connectToMongoDB = async () => {
         await mongoose.connect(process.env.MONGO_DB_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
-        console.log("Error connecting to MOngoDB", error.message);
+        console.log("Error connecting to MongoDB", error.message);
     }
 }
 
